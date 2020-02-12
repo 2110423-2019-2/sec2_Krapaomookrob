4f637adfa0146ace18a9377998fe3334b9939936
NithiP <the-px@hotmail.com>
2020-02-12 18:07:33 +0700

    class style

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -18,8 +18,8 @@
         <div class="d-flex flex-wrap">
           <div class="card p-3 dash mr-3">
               <h5>P'Taan</h5>
-              <span>Saturday, 23 Nov 2019</span>
-              <span>13:00-15:00 (2hrs)</span>
+              <span class="d-block">Saturday, 23 Nov 2019</span>
+              <span class="d-block">13:00-15:00 (2hrs)</span>
               <a class="btn ownbtn" href="#">Start Attendance Checking</a>
           </div>
         </div>
