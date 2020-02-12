ad80e0cfcad7b3a2b8ece359cbd94f206988bc05
Thanawat <taan02991@hotmail.com>
2020-02-12 16:37:51 +0700

    Create BankAccount Schema

---
+++ laravel/app/BankAccount.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BankAccount extends Model
+{
+    protected $guarded = [];
+
+    protected $fillable = [
+        'id', 'account_number', 'account_name', 'bank', 'user_id'
+    ];
+}
---
+++ laravel/database/migrations/2020_02_12_092030_create_bank_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('account_number');
+            $table->string('account_name');
+            $table->string('bank');
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
