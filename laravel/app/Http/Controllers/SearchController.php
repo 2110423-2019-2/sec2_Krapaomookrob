8ceb7a9487b90734225114edabcdc97330ab6884
tongplw <bangkok.tong@gmail.com>
2020-02-12 16:36:39 +0700

    add phpmyadmin

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Course;
 
 class SearchController extends Controller
 {
     
     public function searchCourse(Request $request){
-        dd($request->input('aaa'));
+        // dd($request->input('aaa'));
+        // TODO: 
         return response()->json([
             'name' => 'Abigail',
             'state' => 'CA'
--- laravel/resources/views/searchCourses.blade.php
+++ laravel/resources/views/searchCourses.blade.php
@@ -22,6 +22,12 @@
               <span>13:00-15:00 (2hrs)</span>
               <a class="btn ownbtn" href="#">Start Attendance Checking</a>
           </div>
+          <div class="card p-3 dash mr-3">
+              <h5>P'Taan</h5>
+              <span>Saturday, 23 Nov 2019</span>
+              <span>13:00-15:00 (2hrs)</span>
+              <a class="btn ownbtn" href="#">Start Attendance Checking</a>
+          </div>
         </div>
       </div>
     </div>
