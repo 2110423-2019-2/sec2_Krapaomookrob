130107326358a76ecc2457f83387f57f6268854f
tongplw <bangkok.tong@gmail.com>
2020-02-14 22:18:02 +0700

    edit css:hover bug

--- laravel/resources/views/searchCourses.blade.php
+++ laravel/resources/views/searchCourses.blade.php
@@ -17,6 +17,7 @@
     border-radius: 3px 0px 0px 3px; 
     margin-right: 0px;
     padding: 10px;
+    pointer-events:none;
   }
   .searchInput{
     border: solid 1px rgb(200,200,200); 
@@ -49,7 +50,7 @@
                     <div class="column mx-4">
                         <h5> Tutor</h5>  
                         <div class="row ml-1">
-                          <button class="searchBtn" disabled>
+                          <button class="searchBtn">
                             <svg class="d-flex align-item-center justify-content-center" style="fill: white" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 20 20">
                                 <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                             </svg>
@@ -61,7 +62,7 @@
                         
                         <h5> Area</h5>  
                           <div class="row ml-1">
-                          <button class="searchBtn" disabled>
+                          <button class="searchBtn">
                             <svg class="d-flex align-item-center justify-content-center" style="fill: white" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 20 20">
                                 <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                             </svg>
@@ -76,7 +77,7 @@
                     <div class="column mx-4">
                         <h5>Subjects</h5>  
                         <div class="row ml-1">
-                          <button class="searchBtn" disabled>
+                          <button class="searchBtn">
                             <svg class="d-flex align-item-center justify-content-center" style="fill: white" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 20 20">
                                 <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                             </svg>
