7543e667eb78c19401f34b45762f01d0597b11a3
Unknown <OozyGrub@hotmail.com>
2020-03-09 21:23:22 +0700

    Add Footer

---
+++ laravel/resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id = "footer" style = "background-color: black;color: white">
+  <div class="row">
+    <div class="col-2 offset-1 pt-5">
+      <p><a href="#">Contact Admin</a></p>
+    </div>
+    </div>
+  </div>
+</footer>
