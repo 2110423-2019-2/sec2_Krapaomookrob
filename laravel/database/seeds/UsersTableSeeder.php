9491c6bd8dd1753fb41714715caa13c8ecbcf242
Unknown <OozyGrub@hotmail.com>
2020-04-01 21:47:29 +0700

    Edit Seed Profile Picture

--- laravel/database/seeds/UsersTableSeeder.php
+++ laravel/database/seeds/UsersTableSeeder.php
@@ -43,10 +43,11 @@ class UsersTableSeeder extends Seeder
             'nickname' => 'Somchai'
         ]);
 
+        //  Tutor
         User::create([
             'email' => 'Somchang' .'@gmail.com',
             'name' => 'Somchang Kserasew',
-            'image' => 'https://graph.facebook.com/v3.3/2685763254794575/picture?type=normal',
+            'image' => 'https://youngminds.org.uk/media/1832/teacher-at-whiteboard-opt.jpg?anchor=center&mode=crop&width=2500&quality=80&heightratio=.5625&rnd=131605867010000000',
             'password' => bcrypt(rand(1000, 9999)),
             'role' => 'tutor',
             'balance' => 8000,
@@ -57,7 +58,7 @@ class UsersTableSeeder extends Seeder
         User::create([
             'email' => 'Somry' .'@gmail.com',
             'name' => 'Somry Reswaeq',
-            'image' => 'https://graph.facebook.com/v3.3/2685763254794575/picture?type=normal',
+            'image' => 'https://tefltrainerspain.files.wordpress.com/2013/01/smiling-teacher.jpg',
             'password' => bcrypt(rand(1000, 9999)),
             'role' => 'tutor',
             'balance' => 10000,
@@ -68,7 +69,7 @@ class UsersTableSeeder extends Seeder
         User::create([
             'email' => 'Somwan' .'@gmail.com',
             'name' => 'Somrwan Asedseo',
-            'image' => 'https://graph.facebook.com/v3.3/2685763254794575/picture?type=normal',
+            'image' => 'https://nationalpostcom.files.wordpress.com/2019/11/teacher.jpg?quality=80&strip=all&w=780&zoom=2',
             'password' => bcrypt(rand(1000, 9999)),
             'role' => 'tutor',
             'balance' => 5000,
