05cbe24a1698b56c620946727a1d74b3db9090e5
NithiP <the-px@hotmail.com>
2020-04-18 14:10:53 +0700

    dialogflow

--- laravel/app/Http/Controllers/ChatController.php
+++ laravel/app/Http/Controllers/ChatController.php
@@ -8,6 +8,8 @@ use App\Events\MessageSent;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
+define('DIALOGFLOW_KEY' , env("DIALOGFLOW_KEY", null));
+
 class ChatController extends Controller
 {
     public function getChatList(Request $request)
@@ -32,15 +34,36 @@ class ChatController extends Controller
         //receiver == 1 is AI
         if($receiver != 1){
             broadcast(new MessageSent($message))->toOthers();
+            return ['status' => 'Message Sent!'];
         }else{
-          $dialogflow = "Sawadee";
+          $dialogflow = $this->processOutput($content);
           $reply = Message::create([
                 'content' => $dialogflow,
                 'sender_id' => 1,
                 'receiver_id' => $sender
               ]);
-          broadcast(new MessageSent($reply))->toOthers();
+          return $reply;
         }
-        return ['status' => 'Message Sent!'];
+    }
+
+    public function processOutput($msg){
+      $encode = rawurlencode($msg);
+      $curl = curl_init();
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.dialogflow.com/v1/query?v=20150910&lang=th&query={$encode}&sessionId=12345",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+          "Authorization: Bearer ".DIALOGFLOW_KEY
+        ),
+      ));
+      $response = json_decode(curl_exec($curl), true);
+      curl_close($curl);
+      return $response['result']['fulfillment']['messages'][0]['speech'];
     }
 }
